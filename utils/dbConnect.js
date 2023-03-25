7cd3f3bea3cd65a38257e73813b7c3ff0fcad8ec
DragoonKing <121560774+DragoonKing@users.noreply.github.com>
2023-03-25 12:54:14 +0530

    Update dbConnect.js

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 mongoose.set('strictQuery', false);
 const Connection=()=>{
-mongoose.connect("mongodb+srv://SahilDragoon:Lm4LRGYPgNkO8ur8@cluster0.dlxkkkc.mongodb.net/?retryWrites=true&w=majority",{useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('DB CONNECTED!'))
+mongoose.connect(process.env.MONGO_URI,{useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('DB CONNECTED!'))
 .catch(err => console.log(err));
 }
 module.exports=Connection;
